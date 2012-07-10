bdcf891ff34d91c0c8fcc09a8fe6cfe9542baf3c
Frank de Jonge <info@frenky.net>
2012-07-11 00:56:16 +0200

    Added licence comments and package name

--- classes/Cabinet/Database/Compiler/Sql/Mysql.php
+++ classes/Cabinet/Database/Compiler/Sql/Mysql.php
@@ -1,4 +1,14 @@
 <?php
+/**
+ * Cabinet is an easy flexible PHP 5.3+ Database Abstraction Layer
+ *
+ * @package    Cabinet
+ * @version    1.0
+ * @author     Frank de Jonge
+ * @license    MIT License
+ * @copyright  2011 - 2012 Frank de Jonge
+ * @link       http://cabinetphp.com
+ */
 
 namespace Cabinet\Database\Compiler\Sql;
 
@@ -6,6 +16,12 @@ use Cabinet\Database\Compiler\Sql;
 
 class Mysql extends Sql
 {
+	/**
+	 * Compiles MySQL functions
+	 *
+	 * @param   object  $value  function object
+	 * @return  string  compiles MySQL function
+	 */
 	protected function compilePartFn($value)
 	{
 		$fn = strtoupper($value->getFn());
